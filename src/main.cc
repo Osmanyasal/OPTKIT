5e107e986b5f4ed85f50d8a9a3f4b1bf21185deb
Osman Yasal <osmyasal@gmail.com>
2025-10-05 09:35:18 +0300

    ..

--- src/main.cc
+++ src/main.cc
@@ -18,9 +18,9 @@ inline std::vector<T> generate_vector(size_t n = VECTOR_SIZE)
 int32_t main(int32_t argc, char **argv)
 {
     OPTKIT_INIT();
-    // OPTKIT_HWMON_TEMPERATURE_EVENTS("main", {});
-    // OPTKIT_GPU_TEMPERATURE_EVENTS("main gpu temp", {});
-    // OPTKIT_GPU_ENERGY_EVENTS("main gpu energy", {});
+    OPTKIT_HWMON_TEMPERATURE_EVENTS("main hwmon", {});
+    OPTKIT_GPU_TEMPERATURE_EVENTS("main gpu temp", {});
+    OPTKIT_GPU_ENERGY_EVENTS("main gpu energy", {});
     OPTKIT_CPU_ENERGY("main_block");
     // optkit::gpu::GpuDeviceInfo info;
     // optkit::gpu::Query::device_query(optkit::gpu::GpuVendor::NVIDIA, 0, info);
