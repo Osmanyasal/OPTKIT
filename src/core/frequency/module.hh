a7ce20783208a8609a17f142d02fa7b0fb4c48d3
Osman Yasal <osmyasal@gmail.com>
2025-10-12 17:58:30 +0300

    ..

--- src/core/frequency/module.hh
+++ src/core/frequency/module.hh
@@ -6,14 +6,14 @@
 
 #if OPTKIT_CONF_FREQ_MACROS_ENABLED
 
-#define OPTKIT_SET_CPU_CORE_FREQ(frequency, socket) CPUFrequency::set_core_frequency(frequency, socket)
-#define OPTKIT_SET_CPU_UNCORE_FREQ(frequency, socket) CPUFrequency::set_uncore_frequency(frequency, socket)
+#define OPTKIT_SET_CPU_CORE_FREQ(frequency, socket) Frequency::set_core_frequency(frequency, socket)
+#define OPTKIT_SET_CPU_UNCORE_FREQ(frequency, socket) Frequency::set_uncore_frequency(frequency, socket)
 #define OPTKIT_SET_CPU_FREQ(core_freq, uncore_freq, socket) \
     OPTKIT_SET_CPU_UNCORE_FREQ(core_freq, socket);          \
     OPTKIT_SET_CPU_CORE_FREQ(uncore_freq, socket)
 
-#define OPTKIT_RESET_CPU_CORE_FREQ(socket) CPUFrequency::reset_core_frequency(socket)
-#define OPTKIT_RESET_CPU_UNCORE_FREQ(socket) CPUFrequency::reset_uncore_frequency(socket)
+#define OPTKIT_RESET_CPU_CORE_FREQ(socket) Frequency::reset_core_frequency(socket)
+#define OPTKIT_RESET_CPU_UNCORE_FREQ(socket) Frequency::reset_uncore_frequency(socket)
 #else
 
 #define OPTKIT_SET_CPU_CORE_FREQ(frequency, socket)
