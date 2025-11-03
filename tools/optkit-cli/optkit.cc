7001b56760bd09bf4d7b6dbcd335c62091a6bef1
Osman Yasal <osmyasal@gmail.com>
2025-11-03 18:17:04 +0300

    stream & nas benchmarks added as examples

--- tools/optkit-cli/optkit.cc
+++ tools/optkit-cli/optkit.cc
@@ -3,8 +3,11 @@
 
 int32_t main(int32_t argc, char **argv)
 {
-    OPTKIT_INIT({false});
     CommandArgs args = parse_arguments(argc, argv);
+    if (args.bench_type == BenchType::DEFAULT)
+        OPTKIT_INIT({false});
+    else
+        OPTKIT_INIT({true});
     execute_command(args);
     return 0;
 }
