6660c1ee364466674bfe0c056c5943820300fa70
Osman Yasal <osmyasal@gmail.com>
2026-06-22 10:34:29 +0300

    UPD: increase VECTOR_LEN to 1GB and change GPU events to sampling

--- examples/gpu_kernel_activity/main.cc
+++ examples/gpu_kernel_activity/main.cc
@@ -4,7 +4,7 @@
 
 // CUDA kernel for vector addition
 #ifndef VECTOR_LEN
-#define VECTOR_LEN 1024 * 1024
+#define VECTOR_LEN 1024 * 1024 * 1024
 #endif
 
 extern "C" void test_gpu(int vectorLen = VECTOR_LEN);
@@ -13,7 +13,7 @@ int32_t main(int32_t argc, char **argv)
 {
     OPTKIT_INIT(false);
     {
-        OPTKIT_GPU_EVENTS("gpu_kernel_activity");
+        OPTKIT_GPU_EVENTS_SAMPLING("gpu_kernel_activity");
         test_gpu();
     }
     return 0;
